1eeb622cb206ce1b3075ce3e895d6b87b07c7aac
Matthew Nelson <05nelsonm@protonmail.com>
2019-08-15 19:53:46 -0400

    added pending intent to open to fullscreen activity

--- app/src/main/java/sats/stackemhigh/androidnotifications/MainActivity.kt
+++ app/src/main/java/sats/stackemhigh/androidnotifications/MainActivity.kt
@@ -16,22 +16,25 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         const val NOTIFICATION_ID = 3871
+        const val NOTIFICATION_KEY = "PLAKJSD09F2I9FUHB1Q"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_get_notification.setOnClickListener { p0 ->
-            // Create an intent for the notification to open the app
-            val contentIntent = Intent(this, MainActivity::class.java)
+        btn_get_notification.setOnClickListener {
+            // Create an intent for the notification to open the app to the FullscreenActivity
+            val contentIntent = Intent(this, FullscreenActivity::class.java)
+            contentIntent.putExtra(NOTIFICATION_KEY, "Notification Tapped")
             val pendingContentIntent = PendingIntent.getActivity(this, 0, contentIntent, PendingIntent.FLAG_ONE_SHOT)
 
-            // Channel ID (needed outside of `if` statements)
+            // Channel ID (needed outside of `if` statements checking for version)
             val channelId = "$packageName.simplechan"
+
             val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-            // Check for build version greater than or equal to version Oreo
+            // Check for build version greater than or equal to version Oreo to set channels
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 val name = "Channel Notification"
                 val importance = NotificationManager.IMPORTANCE_LOW
@@ -51,6 +54,7 @@ class MainActivity : AppCompatActivity() {
                 .setSmallIcon(android.R.drawable.ic_dialog_alert)
                 .setColor(Color.GREEN)
                 .setDefaults(1)
+                .setContentIntent(pendingContentIntent)
             notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
         }
 
