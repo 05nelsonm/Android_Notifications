59f9cc7b245b9302cec02e190749411ee2813f8f
Matthew Nelson <05nelsonm@protonmail.com>
2019-08-15 16:08:46 -0400

    cleaned up MainActivity

--- app/src/main/java/sats/stackemhigh/androidnotifications/MainActivity.kt
+++ app/src/main/java/sats/stackemhigh/androidnotifications/MainActivity.kt
@@ -2,7 +2,6 @@ package sats.stackemhigh.androidnotifications
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.app.NotificationManager.IMPORTANCE_HIGH
 import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_ONE_SHOT
 import android.content.Context
@@ -24,9 +23,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val NotificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        btn_get_notification.setOnClickListener {
+        btn_get_notification.setOnClickListener { p0 ->
             // Create an intent for the notification to open the app
             val contentIntent = Intent(this, MainActivity::class.java)
             val pendingContentIntent = PendingIntent.getActivity(this, 0, contentIntent, FLAG_ONE_SHOT)
@@ -38,7 +35,7 @@ class MainActivity : AppCompatActivity() {
             // Check for build version greater than or equal to version Oreo
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 val name = "Channel Notification"
-                val importance = IMPORTANCE_HIGH
+                val importance = NotificationManager.IMPORTANCE_HIGH
                 val description = "Description Description Description"
 
                 val channel = NotificationChannel(channelId, name, importance)
@@ -49,7 +46,7 @@ class MainActivity : AppCompatActivity() {
 
             // Will always happen independent of build version
             val notificationBuilder = NotificationCompat.Builder(this, channelId)
-                .setPriority(IMPORTANCE_HIGH)
+                .setPriority(NotificationManager.IMPORTANCE_HIGH)
                 .setContentTitle("Notification Notification")
                 .setContentText("Content Content Content")
                 .setSmallIcon(android.R.drawable.ic_dialog_alert)
